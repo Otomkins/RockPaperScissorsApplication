4bb864ca235c6677f20e07e0578d80c86e54f27c
Oliver Tomkins <otomkins@yahoo.co.uk>
2021-01-18 15:18:36 +0000

    Seperated Code Into Service Layer & Intergrated Into xaml

--- Rock_Paper_Scissors_Code/RockPaperScissorsCode.cs
+++ Rock_Paper_Scissors_Code/RockPaperScissorsCode.cs
@@ -6,92 +6,24 @@ namespace Rock_Paper_Scissors_Code
 {
     public class RockPaperScissorsCode
     {
-        public int player1Score;
-        public int player2Score;
+        readonly RockPaperScissorsService _rpss = new RockPaperScissorsService();
 
-        public string player1Choice;
-        public string player2Choice;
+        public void SetPlayerChoice(int playerChoice) => _rpss.SetPlayerChoice(playerChoice);
 
-        public string[] choices = new string[6]
-        {
-            "Rock",
-            "Paper",
-            "Scissors",
-            "Rock",
-            "Paper",
-            "Scissors"
-        };
+        public int RetrievePlayer1Score() => _rpss.RetrievePlayer1Score();
 
-        public void SetPlayerChoice(int playerChoice)
-        {
-            if (playerChoice > 0 && playerChoice < 4)
-                player1Choice = choices[playerChoice - 1];
-            else
-                player2Choice = choices[playerChoice - 1];
-        }
+        public int RetrievePlayer2Score() => _rpss.RetrievePlayer2Score();
 
-        public string DetermineWinner()
-        {
-            string winnerMessage = "";
+        public string RetrievePlayer1Choice() => _rpss.RetrievePlayer1Choice();
 
-            switch (DetermineOutcome())
-            {
-                // Player 1 Wins
-                case 1:
-                    player1Score++;
-                    winnerMessage = "Player 1 Wins! Rock Beats Scissors!";
-                    break;
-                case 2:
-                    player1Score++;
-                    winnerMessage = "Player 1 Wins! Paper Beats Rock!";
-                    break;
-                case 3:
-                    player1Score++;
-                    winnerMessage = "Player 1 Wins! Scissors Beats Paper!";
-                    break;
-                // Player 2 Wins
-                case 4:  
-                    player2Score++;
-                    winnerMessage = "Player 2 Wins! Paper Beats Rock!";
-                    break;
-                case 5:
-                    player2Score++;
-                    winnerMessage = "Player 2 Wins! Scissors Beats Paper!";
-                    break;
-                case 6:
-                    player2Score++;
-                    winnerMessage = "Player 2 Wins! Rock Beats Scissors!";
-                    break;
-                // Draw
-                case 7:
-                    winnerMessage = "Draw! Both Players Selected Rock!";
-                    break;
-                case 8:
-                    winnerMessage = "Draw! Both Players Selected Paper!";
-                    break;
-                case 9:
-                    winnerMessage = "Draw! Both Players Selected Scissors!";
-                    break;
-            }
-            return winnerMessage;
-        }
+        public string RetrievePlayer2Choice() => _rpss.RetrievePlayer2Choice();
 
-        private int DetermineOutcome()
-        {
-            // Player 1 Wins
-            if (player1Choice == "Rock" && player2Choice == "Scissors") return 1;
-            else if (player1Choice == "Paper" && player2Choice == "Rock") return 2;
-            else if (player1Choice == "Scissors" && player2Choice == "Paper") return 3;
+        public string RetrieveOutcome() =>  _rpss.DetermineWinner();
 
-            // Player 2 Wins
-            else if (player1Choice == "Rock" && player2Choice == "Paper") return 4;
-            else if (player1Choice == "Paper" && player2Choice == "Scissors") return 5;
-            else if (player1Choice == "Scissors" && player2Choice == "Rock") return 6;
 
-            // Draw
-            else if (player1Choice == "Rock" && player2Choice == "Rock") return 7;
-            else if (player1Choice == "Paper" && player2Choice == "Paper") return 8;
-            else return 9;
-        }
+
+
+
+
     }
 }
--- Rock_Paper_Scissors_WPF/MainWindow.xaml.cs
+++ Rock_Paper_Scissors_WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Rock_Paper_Scissors_Code;
 
 namespace Rock_Paper_Scissors_WPF
 {
@@ -20,28 +21,64 @@ namespace Rock_Paper_Scissors_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        RockPaperScissorsCode _rpsc = new RockPaperScissorsCode();
 
         BitmapImage _player1RockImage = new BitmapImage(new Uri("/Images/Player_1_Rock.png", UriKind.Relative));
         BitmapImage _player1PaperImage = new BitmapImage(new Uri("/Images/Player_1_Paper.png", UriKind.Relative));
         BitmapImage _player1ScissorsImage = new BitmapImage(new Uri("/Images/Player_1_Scissors.png", UriKind.Relative));
 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
         private void Player_1_Selection_Rock_Button_Click(object sender, RoutedEventArgs e)
         {
             Player_1_Choice_Image.Source = _player1RockImage;
+            DisplayFinalOutcome(1);
+
         }
 
         private void Player_1_Selection_Paper_Button_Click(object sender, RoutedEventArgs e)
         {
             Player_1_Choice_Image.Source = _player1PaperImage;
+            DisplayFinalOutcome(2);
         }
 
         private void Player_1_Selection_Scissors_Button_Click(object sender, RoutedEventArgs e)
         {
             Player_1_Choice_Image.Source = _player1ScissorsImage;
+            DisplayFinalOutcome(3);
+        }
+
+        private void DisplayFinalOutcome(int player1Choice)
+        {
+            _rpsc.SetPlayerChoice(player1Choice);
+            Round_Log_TextBox.Text = _rpsc.RetrieveOutcome();
+            Player_1_Score_TextBox.Text = _rpsc.RetrievePlayer1Score().ToString();
+            Player_2_Score_TextBox.Text = _rpsc.RetrievePlayer2Score().ToString();
+            SetPlayer2Choice();
+        }
+
+        private void SetPlayer2Choice()
+        {
+            switch (_rpsc.RetrievePlayer2Choice())
+            {
+                case "Rock":
+                    Player_2_Choice_Image.Source = new BitmapImage(new Uri("/Images/Player_2_Rock.png", UriKind.Relative));
+                    break;
+                case "Paper":
+                    Player_2_Choice_Image.Source = new BitmapImage(new Uri("/Images/Player_2_Paper.png", UriKind.Relative));
+                    break;
+                case "Scissors":
+                    Player_2_Choice_Image.Source = new BitmapImage(new Uri("/Images/Player_2_Scissors.png", UriKind.Relative));
+                    break;
+            }
+        }
+
+        private void Game_Restart_Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
